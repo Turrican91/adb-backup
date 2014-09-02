92fdc5db68ae914c9b9d75d7d625d8fcb22b5656
Denis <denis.schubhan@freenet.de>
2014-09-02 13:48:17 +0200

    Ausgabe der wichtigen Pfade

--- adb-backup-linux.sh
+++ adb-backup-linux.sh
@@ -6,3 +6,6 @@ backupdate=`date +%Y_%m_%d`
 
 #Set path to Android-SDK
 androidsdk="/path/to/sdk/"
+
+echo "Aktuelles Datum: " $backupdate
+echo "ADB-Pfad: " $androidsdk "platform-tools/adb"
--- adb-backup-windows.cmd
+++ adb-backup-windows.cmd
@@ -6,3 +6,6 @@ set backupdate=%date:~6,4%_%date:~3,2%_%date:~0,2%
 
 #Set path to Android-SDK
 set androidsdk="C:\path\to\sdk\"
+
+echo "Aktuelles Datum: " %backupdate%
+echo "ADB-Pfad: " %androidsdk% "platform-tools\adb.exe"
