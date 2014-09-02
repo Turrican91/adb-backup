f985b0746363cd1ff177d94ff7f669bb3b456a15
Denis <denis.schubhan@freenet.de>
2014-09-02 13:43:25 +0200

    Added "backupdate" and "androidsdk" variable

--- adb-backup-linux.sh
+++ adb-backup-linux.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+#Declaration of Variables
+
 backupdate=`date +%Y_%m_%d`
+
+#Set path to Android-SDK
+androidsdk="/path/to/sdk/"
--- adb-backup-windows.cmd
+++ adb-backup-windows.cmd
@@ -1,3 +1,8 @@
 @echo on
 
+#Declaration of Variables
+
 set backupdate=%date:~6,4%_%date:~3,2%_%date:~0,2%
+
+#Set path to Android-SDK
+set androidsdk="C:\path\to\sdk\"
